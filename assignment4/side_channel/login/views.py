aa44842fb68c2b4259515032373e8203941a385a
Alec Mitchell-Thomson <alec.mitchellthomson@gmail.com>
2026-06-11 12:45:16 +0100

    assignment 4 done!

--- assignment4/side_channel/login/views.py
+++ assignment4/side_channel/login/views.py
@@ -45,9 +45,11 @@ def safe_check(username: str, password: str) -> bool:
         # then replace _vulnerable_check with safe_check in login_view() below
         if i >= len(SECRET_PASSWORD) or ch != SECRET_PASSWORD[i]:
             return False
-        time.sleep(DELAY_PER_CHAR)
 
-    return len(password) == len(SECRET_PASSWORD)
+    if len(password) == len(SECRET_PASSWORD):
+        return True
+    else:
+        return False
 
 
 
@@ -61,7 +63,7 @@ def login_view(request):
         password = request.POST.get("password", "")
 
         t0 = time.perf_counter()
-        success = _vulnerable_check(username, password) # replace with safe_check as neede!
+        success = safe_check(username, password) # replace with safe_check as neede!
         elapsed_ms = (time.perf_counter() - t0) * 1000
 
         if success:
